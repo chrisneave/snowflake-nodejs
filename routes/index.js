1c2b393ad0e782783cf4207fedf18f70f2d7da62
Chris Neave <chrispneave@gmail.com>
2014-06-27 21:29:42 +0100

    Nav menu, login and welcome views
    Fix missing title variable
    Login and welcome views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var title = 'Snowflake Node.js App';
+
+  if (res.user) {
+    res.render('welcome', {
+      title: title,
+      message: util.format('Hi %s %s!', user.given_name, user.family_name)
+    });
+  } else {
+    res.render('login', {
+      title: title,
+      message: util.format('To use the %s please login by clicking on the link below', title)
+    });
+  }
 });
 
 module.exports = router;
