0d7d463640b0676ac5664324a45a0b8b23b688ca
Chris Neave <chrispneave@gmail.com>
2014-06-30 21:07:08 +0100

    End the user info request
    Tidied up the token request code

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ function getUserInfo(access_token, id_token, done) {
 
   request = https.request({
     hostname: 'snowflake-op.herokuapp.com',
-    port: 433,
+    port: 443,
     path: '/userinfo',
     method: 'GET',
     headers: {
@@ -63,9 +63,47 @@ function getUserInfo(access_token, id_token, done) {
 
     res.on('end', function() {
       var response_json = JSON.parse(response_data);
-      done(response_data);
+      done(null, response_json);
     });
   });
+
+  request.end();
+};
+
+function getToken(code, done) {
+  var request,
+      post_data = {};
+
+  post_data.client_id = 'snowflake-nodejs';
+  post_data.grant_type = 'authorization_code';
+  post_data.redirect_uri = 'https://snowflake-nodejs.herokuapp.com/user/login/openid/snowflake';
+  post_data.code = code;
+
+  request = https.request({
+    hostname: 'snowflake-op.herokuapp.com',
+    port: 443,
+    path: '/oauth/token',
+    method: 'POST',
+    auth: 'snowflake-nodejs:7796f01f7251dc47f440ae461ac2fc8d'
+  }, function(res) {
+    var response_data = '';
+    res.setEncoding('utf8');
+    res.on('data', function(data) {
+      response_data += data;
+    });
+    res.on('end', function() {
+      var response_json = JSON.parse(response_data);
+      if (response_json.error) { return done(response_json); }
+
+      var access_token = response_json.access_token,
+          id_token = jwt.decode(response_json.id_token, 'https://snowflake-op.herokuapp.com');
+
+      getUserInfo(access_token, id_token, done);
+    });
+  });
+
+  request.write(qs.stringify(post_data));
+  request.end();
 };
 
 router.get('/user/login/openid/snowflake', function(req, res) {
@@ -73,45 +111,17 @@ router.get('/user/login/openid/snowflake', function(req, res) {
     res.render('error', { message: util.format('%s', req.params['error']) })
   }
 
-  var request,
-      post_data = {};
-
-  post_data.client_id = 'snowflake-nodejs';
-  post_data.grant_type = 'authorization_code';
-  post_data.redirect_uri = 'https://snowflake-nodejs.herokuapp.com/user/login/openid/snowflake';
-  post_data.code = req.params['code'];
-
-  request = https.request({
-    hostname: 'snowflake-op.herokuapp.com',
-    port: 433,
-    path: '/oauth/token',
-    method: 'POST',
-    auth: 'snowflake-nodejs:7796f01f7251dc47f440ae461ac2fc8d'
-  }, function(res) {
-    var reponse_data = '';
-    res.setEncoding('utf8');
-    res.on('data', function(data) {
-      response_data += data;
-    });
-    res.on('end', function() {
-      if (response_data.error) {
-        res.render('error', {
-          message: util.format('%s - %s', response_data.error, response_data.error_description)
-        });
-      }
-
-      var access_token = response_data.access_token,
-          id_token = jwt.decode(response_data.id_token, 'https://snowflake-op.herokuapp.com');
-
-      getUserInfo(access_token, id_token, function(response_json) {
-        req.session['user'] = response_json;
-        res.redirect('/');
+  getToken(req.query.code, function(err, result) {
+    if (err) {
+      res.render('error', {
+        message: util.format('%s - %s', err.error, err.error_description || '')
       });
-    });
+      res.end();
+    } else {
+      req.session['user'] = result;
+      res.redirect('/');
+    }
   });
-
-  request.write(qs.stringify(post_data));
-  request.end();
 });
 
 module.exports = router;
